927ef3ad513b8bc4722cccb41d14e037833dd853
ai-llm-inder <withinders@gmail.com>
2025-06-12 13:58:12 +1000

    filerevert in gitrepo module

--- github-repo/main.tf
+++ github-repo/main.tf
@@ -1,1 +1,13 @@
 
+resource "github_repository" "this" {
+  name        = var.name
+  description = var.description
+  private     = var.private
+
+  visibility  = var.private ? "private" : "public"
+
+  has_issues    = true
+  has_wiki      = false
+  auto_init     = true
+  delete_branch_on_merge = true
+}
