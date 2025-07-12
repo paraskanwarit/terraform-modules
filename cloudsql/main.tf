05d26bf453d033ca8d77af92dce447c0d6f3b724
paraskanwarit <142704151+paraskanwarit@users.noreply.github.com>
2025-07-12 22:26:22 +1000

    Update main.tf

--- cloudsql/main.tf
+++ cloudsql/main.tf
@@ -1,42 +1,46 @@
-// See: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance
-
 resource "google_sql_database_instance" "primary" {
   name             = var.instance_name
   database_version = var.database_version
   region           = var.region
 
   settings {
-    tier                        = var.tier
-    availability_type           = var.availability_type
-    disk_autoresize             = true
-    disk_size                   = var.disk_size
-    disk_type                   = var.disk_type
+    tier              = var.tier
+    availability_type = var.availability_type
+    disk_autoresize   = true
+    disk_size         = var.disk_size
+    disk_type         = var.disk_type
+
     backup_configuration {
-      enabled                        = true
-      binary_log_enabled             = true
-      start_time                     = "03:00"
+      enabled            = true
+      binary_log_enabled = true
+      start_time         = "03:00"
     }
+
     ip_configuration {
       ipv4_enabled    = var.ipv4_enabled
       private_network = var.private_network
-      require_ssl     = true
-      authorized_networks = var.authorized_networks
+
+      dynamic "authorized_networks" {
+        for_each = var.authorized_networks
+        content {
+          name  = authorized_networks.value.name
+          value = authorized_networks.value.value
+        }
+      }
     }
+
     maintenance_window {
       day          = 7
       hour         = 3
       update_track = "stable"
     }
-    activation_policy = "ALWAYS"
-    deletion_protection_enabled = true
+
+    activation_policy             = "ALWAYS"
+    deletion_protection_enabled  = true
+    user_labels                  = var.labels
   }
 
-  root_password = var.root_password
-
   deletion_protection = true
-
-  // Add labels for environment, owner, etc.
-  labels = var.labels
 }
 
 resource "google_sql_user" "default" {
